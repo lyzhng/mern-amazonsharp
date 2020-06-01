7bca2fc971d63940764704a0c41731551777523d
Lily Zhong <lilyzhongy@gmail.com>
2020-06-01 16:16:39 -0400

    add form validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,52 @@
 const express = require('express');
+const { check, body, validationResult } = require('express-validator');
 const router = express.Router();
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 
 const { User } = require('../models');
 
-const HASH_ITERATIONS = 10;
+router.post('/register', [
+  body('username').isLength({ min: 6, max: 16 }).isAlphanumeric(),
+  body('email').isEmail(),
+  body('password').isLength({ min: 8 }),
+  body('email').custom(async (email) => {
+    const user = await User.findOne({ email });
+    if (user) {
+      return Promise.reject('E-mail is already in use.')
+    }
+  }),
+  body('username').custom(async (username) => {
+    const user = await User.findOne({ username });
+    if (user) {
+      return Promise.reject('Username is already in use.')
+    }
+  }),
+], async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() });
+  }
+  const { email, username, password } = req.body;
+
+  bcrypt.hash(password, process.env.HASH_ITER, async (err, hash) => {
+    if (err) {
+      return res.status(500).json({
+        err: 'An error has occurred during while hashing the password.',
+      });
+    }
+    await User.create({ username, email, password: hash, });
+    res.status(200).json({ redirectLoc: '/login' });
+  })
+});
+
+router.post('/login', passport.authenticate('local'), (req, res, next) => {
+  console.log(req.user)
+  return res.status(200).json({
+    message: 'Login successful.',
+    redirectLoc: '/'
+  })
+});
 
 router.post('/logout', (req, res) => {
   req.session.destroy(err => {
@@ -18,39 +59,5 @@ router.post('/logout', (req, res) => {
     })
   })
 })
-
-router.post('/register', async (req, res) => {
-  const { username, email, password } = req.body;
-  const [userWithUsername, userWithEmail] = await Promise.all([User.findOne({ email }), User.findOne({ username })]);
-
-  if (!userWithUsername && !userWithEmail) {
-    bcrypt.hash(password, HASH_ITERATIONS, async (err, hash) => {
-      if (err) {
-        console.log('An error has occurred during while hashing the password.');
-        return res.status(500).json({ err });
-      }
-      await User.create({
-        username,
-        email,
-        password: hash,
-      });
-      res.status(200).json({
-        redirectLoc: '/login',
-      });
-    });
-  } else {
-    return res.status(409).json({
-      message: 'User with username or email already exists!',
-    });
-  }
-});
-
-router.post('/login', passport.authenticate('local'), (req, res, next) => {
-  console.log(req.user)
-  return res.status(200).json({
-    message: 'Login successful.',
-    redirectLoc: '/'
-  })
-});
 
 module.exports = router;
