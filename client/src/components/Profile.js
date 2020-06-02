804a6eba86206b32d327222507ca0ef8a1e7b9fc
Lily Zhong <lilyzhongy@gmail.com>
2020-06-02 19:45:50 -0400

    add error handling and functionality for deleting and updating products client-side

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
 import { Container } from "react-bootstrap";
 
 import Navbar from "./Navbar";
 
 const Profile = props => {
-  const [products, setProducts] = React.useState([]);
+  const [products, setProducts] = useState([]);
+  const [editState, setEditState] = useState(false);
+  const [currentProduct, setCurrentProduct] = useState(null);
+
   const username = props.match.params.username;
 
-  React.useEffect(() => {
+  useEffect(() => {
     async function fetchProducts() {
-      const res = await Axios.get(`/api/store/${username}`);
-      setProducts(res.data.products);
+      try {
+        const res = await Axios.get(`/api/store/${username}`);
+        setProducts(res.data.products);
+      } catch (err) {
+        console.error(err);
+      }
     }
     fetchProducts();
-  }, []);
+  }, [setProducts, username]);
 
   return (
     <div className="Wrapper">
@@ -25,9 +32,67 @@ const Profile = props => {
         {
           products.map(product => {
             return (
-              <div>
-                <h4>{product.name}</h4>
-                <h4>{product.price}</h4>
+              <div key={product._id}>
+                <input 
+                  type="text" 
+                  disabled={ !editState } 
+                  value={ currentProduct !== null && currentProduct._id === product._id ? currentProduct.name : product.name } 
+                  onChange={({ target }) => {
+                    console.log('Updated Name', target.value);
+                    setCurrentProduct({
+                      _id: product._id,
+                      name: target.value,
+                      price: (currentProduct !== null && currentProduct._id === product._id) ? currentProduct.price : product.price
+                    })
+                  }}
+                /> 
+                <input 
+                  type="text"
+                  disabled={ !editState }
+                  value={ 
+                    (currentProduct !== null && currentProduct._id === product._id)
+                      ? currentProduct.price 
+                      : product.price
+                  } 
+                  onChange={({ target }) => {
+                    console.log('Updated Price', target.value);
+                    setCurrentProduct({
+                      _id: product._id,
+                      name: (currentProduct !== null && currentProduct._id === product._id) ? currentProduct.name : product.name,
+                      price: target.value,
+                    })
+                  }}
+                /> 
+                <button 
+                  onClick={async () => {
+                    try {
+                      await Axios.delete('/api/products/delete', {
+                        data: { productId: product._id }
+                      });
+                      setProducts(products.filter(p => p._id !== product._id));
+                    } catch (err) {
+                      console.error(err);
+                    }
+                  }}
+                >Delete
+                </button>
+                <button
+                  onClick={async () => {
+                    if (editState) {
+                      try {
+                        await Axios.put('/api/products/update', {
+                          productId: product._id,
+                          updatedPrice: currentProduct.price,
+                          updatedName: currentProduct.name,
+                        });
+                      } catch (err) {
+                        console.error(err);
+                      }
+                    } 
+                    setEditState(!editState);
+                  }}>
+                { editState ? 'Save Changes' : 'Edit' }
+                </button>
               </div>
             )
           })
