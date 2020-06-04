19c021dd73e1e51ec3ba0e7992d3369b8f51fc3a
Lily Zhong <lilyzhongy@gmail.com>
2020-06-04 16:18:30 -0400

    move context out of switch, redirect to not-found when unknown route is hit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import {
   HomePage,
   AboutPage,
@@ -8,8 +8,9 @@ import {
   ExplorePage,
   LoginPage,
   RegisterPage,
+  PostItemPage,
   Profile
-} from './components/index';
+} from './components';
 import { UserContext } from './context/UserContext';
 
 export default () => {
@@ -17,18 +18,23 @@ export default () => {
 
   return (
     <BrowserRouter>
-      <Switch>
-        <UserContext.Provider value={{ user, setUser }}>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route path='/about' component={AboutPage} />
-          <Route path='/contact' component={ContactPage} />
+          <Route exact path='/about' component={AboutPage} />
+          <Route exact path='/contact' component={ContactPage} />
           <Route exact path='/explore' component={ExplorePage} />
           <Route exact path='/login' component={LoginPage} />
           <Route exact path='/register' component={RegisterPage} />
           <Route exact path='/profile/:username' component={Profile} />
-        </UserContext.Provider>
-        <Route exact path='*' component={NotFoundPage} />
-      </Switch>
+          <Route exact path='/product/new' component={PostItemPage} />
+          <Route exact path='/product/:id' component={HomePage} />
+          <Route exact path='/not-found' component={NotFoundPage} />
+          <Route exact path='*'>
+            <Redirect to='/not-found' />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
     </BrowserRouter>
   )
 }
