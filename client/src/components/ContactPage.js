4c7ca69537113cdf149543286a4557b659108109
Lily Zhong <lilyzhongy@gmail.com>
2020-06-23 23:49:36 -0400

    Rewrite ContactPage using blueprintjs

--- client/src/components/ContactPage.js
+++ client/src/components/ContactPage.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import { Button, Form, Container } from 'react-bootstrap';
+import React, { useRef, useState } from 'react';
+import { FormGroup, InputGroup, Button, TextArea, Intent } from '@blueprintjs/core';
 
 import Navbar from './Navbar';
 
-const ContactForm = (
-  <Form onSubmit={e => e.preventDefault()}>
-    <Form.Group>
-      <Form.Label>Name</Form.Label>
-      <Form.Control name="name" type="text" size="sm" />
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Email</Form.Label>
-      <Form.Control name="email" type="email" size="sm"/>
-    </Form.Group>
-    <Form.Group>
-      <Form.Label>Password</Form.Label>
-      <Form.Control name="password" type="password" size="sm"/>
-    </Form.Group>
-    <Button variant="primary" size="sm">Send Message</Button>
-  </Form>
-);
-
 export default () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const textRef = useRef();
+  const [message, setMessage] = useState();
+
+  const contact = async (e) => {
+    e.preventDefault();
+  }
+
+  const contactForm = (
+    <form onSubmit={e => contact(e)} method="POST" noValidate style={{ width: '100%', height: '100%' }}>
+      <FormGroup
+        label="Email"
+        labelFor="email">
+        <InputGroup id="email" name="email" placeholder="johnappleseed@example.com" type="email" inputRef={emailRef}/>
+      </FormGroup>
+      <FormGroup
+        label="Full Name"
+        labelFor="name">
+        <InputGroup id="name" name="name" type="text" inputRef={nameRef}/>
+      </FormGroup>
+      <TextArea intent={Intent.PRIMARY} large fill />
+      <Button intent="primary" type="submit" text="Contact" />
+    </form>
+  );
+
   return (
-    <div className="Wrapper">
+    <div className="Wrapper" style={{ width: '100vw', height: '100vh' }}>
       <Navbar />
-      <Container className="About my-3">
-        <h2>Contact Us</h2>
-        {ContactForm}
-      </Container>
+      <div className="Content" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '1.5rem' }}>
+        {message}
+        {contactForm}
+      </div>
     </div>
   )
 }
