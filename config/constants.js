eb50b74dc7b9b5e47df5898cdea49a9280a34d17
Lily Zhong <lilyzhongy@gmail.com>
2020-06-25 07:44:43 -0400

    refactor #6

---
+++ config/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  USERNAME_IN_USE_MSG: 'Username is already in use.',
+  EMAIL_IN_USE_MSG: 'E-mail is already in use.',
+  EMAIL_OR_USERNAME_EXISTS_MSG: 'E-mail or username already exists.',
+  AUTH_SUCCESS_MSG: 'Authentication successful.',
+}
