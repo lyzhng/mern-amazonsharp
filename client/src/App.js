187feaa6e7d4ca234a71889aac06de9f2ff74dd7
Lily Zhong <lilyzhongy@gmail.com>
2020-07-02 21:15:55 -0400

    Add public and private routes client-side

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import {
   HomePage,
@@ -12,29 +12,45 @@ import {
   Profile
 } from './components';
 import { UserContext } from './context/UserContext';
+import PrivateRoute from './components/PrivateRoute';
+import PublicRoute from './components/PublicRoute';
+import axios from 'axios';
 
-export default () => {
-  const [user, setUser] = useState(null);
+const App = () => {
+  const [user, setUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios
+      .get('/api/profiles/user-data')
+      .then(res => {
+        const { username } = res.data;
+        setUser(username);
+        setLoading(false);
+      });
+    }, []);
 
   return (
     <BrowserRouter>
       <UserContext.Provider value={{ user, setUser }}>
         <Switch>
-          <Route exact path='/' component={HomePage} />
-          <Route exact path='/about' component={AboutPage} />
-          <Route exact path='/contact' component={ContactPage} />
-          <Route exact path='/explore' component={ExplorePage} />
-          <Route exact path='/login' component={LoginPage} />
-          <Route exact path='/register' component={RegisterPage} />
-          <Route exact path='/profile/:username' component={Profile} />
-          <Route exact path='/product/new' component={PostItemPage} />
-          <Route exact path='/product/:id' component={HomePage} />
-          <Route exact path='/not-found' component={NotFoundPage} />
-          <Route exact path='*'>
+          <PublicRoute exact path='/' loading={loading} component={HomePage} />
+          <PublicRoute exact path='/about' loading={loading} component={AboutPage} />
+          <PublicRoute exact path='/contact' loading={loading} component={ContactPage} />
+          <PublicRoute exact path='/explore' loading={loading} component={ExplorePage} />
+          <PublicRoute exact path='/login' loading={loading} component={LoginPage} />
+          <PublicRoute exact path='/register' loading={loading} component={RegisterPage} />
+          <PublicRoute exact path='/profile/:username' loading={loading} component={Profile} />
+          <PrivateRoute exact path='/product/new' loading={loading} component={PostItemPage} />
+          <PublicRoute exact path='/product/:id' loading={loading} component={HomePage} />
+          <PublicRoute exact path='/not-found' loading={loading} component={NotFoundPage} />
+          <PublicRoute exact path='*'>
             <Redirect to='/not-found' />
-          </Route>
+          </PublicRoute>
         </Switch>
       </UserContext.Provider>
     </BrowserRouter>
   )
 }
+
+export default App;
--- client/src/components/PublicRoute.js
+++ client/src/components/PublicRoute.js
@@ -1,20 +1,18 @@
 import React, { useContext, useEffect } from 'react';
-import { Route, Redirect } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 import { UserContext } from '../context/UserContext';
 import Spinner from '../services/Spinner';
 
-const PrivateRoute = ({ component: Component, loading, ...rest }) => {
+const PublicRoute = ({ component: Component, loading, ...rest }) => {
   const { user } = useContext(UserContext);
 
   return (
     <Route {...rest} render={props => (
-      !loading && user
+      !loading 
         ? <Component {...props} />
-        : !loading && !user
-        ? <Redirect to='/login' />
         : Spinner
     )} />
   )
 }
 
-export default PrivateRoute;
+export default PublicRoute;
