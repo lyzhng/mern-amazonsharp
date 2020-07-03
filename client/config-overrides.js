f420ae0c8698e558dfb2e0d3537a6d2612a977e5
Lily Zhong <lilyzhongy@gmail.com>
2020-07-02 21:21:59 -0400

    Add Webpack configuration

---
+++ client/config-overrides.js
@@ -0,0 +1,9 @@
+module.exports = function override(config, env) {
+  console.log('override!');
+  let loaders = config.module.rules[2].oneOf;
+  loaders.splice(loaders.length - 1, 0, {
+    test: /\.scss$/,
+    use: ['style-loader', 'css-loader', 'sass-loader']
+  });
+  return config;
+}
